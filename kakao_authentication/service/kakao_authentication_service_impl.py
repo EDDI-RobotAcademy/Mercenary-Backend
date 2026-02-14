a1e1b2f5d88af9c57e87179cd37e581aa15f059c
Sanghoon Lee <gcccompil3r@gmail.com>
2026-02-14 10:55:11 +0900

    update: [EDDI] 사용자가 Kakao 인증 요청 시 인증 URL 생성 [MB-2]

--- kakao_authentication/service/kakao_authentication_service_impl.py
+++ kakao_authentication/service/kakao_authentication_service_impl.py
@@ -1,12 +1,13 @@
 from urllib.parse import urlencode
-
-from config.settings import settings
-from kakao_authentication.service.kakao_authentication_service import KakaoAuthenticationService
+from config.settings import get_settings
+from kakao_authentication.service.kakao_authentication_service import (
+    KakaoAuthenticationService,
+)
 
 
 class KakaoAuthenticationServiceImpl(KakaoAuthenticationService):
-    __instance = None
 
+    __instance = None
     AUTH_BASE_URL = "https://kauth.kakao.com/oauth/authorize"
 
     def __new__(cls):
@@ -18,9 +19,12 @@ class KakaoAuthenticationServiceImpl(KakaoAuthenticationService):
     def getInstance(cls):
         if cls.__instance is None:
             cls.__instance = cls()
+            
         return cls.__instance
 
     def generate_oauth_url(self) -> str:
+        settings = get_settings()
+
         params = {
             "client_id": settings.KAKAO_CLIENT_ID,
             "redirect_uri": settings.KAKAO_REDIRECT_URI,
