8590151c68062fcc4b7ad57a3c2b1442580081da
Sanghoon Lee <gcccompil3r@gmail.com>
2026-02-18 07:18:45 +0900

    add: [EDDI] 발급 받은 액세스 토큰으로 Kakao 사용자 정보를 조회 [MB-5]

--- kakao_authentication/service/kakao_authentication_service.py
+++ kakao_authentication/service/kakao_authentication_service.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from account.domain.entity.account import Account
+from kakao_authentication.service.response.kakao_login_response import KakaoLoginResponse
 from kakao_authentication.service.response.kakao_token_response import KakaoTokenResponse
 
 
@@ -11,4 +13,12 @@ class KakaoAuthenticationService(ABC):
 
     @abstractmethod
     def request_access_token(self, code: str) -> KakaoTokenResponse:
+        pass
+
+    @abstractmethod
+    def get_user_info(self, access_token: str) -> Account:
+        pass
+
+    @abstractmethod
+    def login_with_kakao(self, code: str) -> KakaoLoginResponse:
         pass
